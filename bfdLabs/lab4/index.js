0cee88931ef8379df5e7b5c2c5ef474631f53069
Artyom Zuev <delm0n@mail.ru>
2021-03-30 23:47:30 +0300

    commit

--- bfdLabs/lab4/index.js
+++ bfdLabs/lab4/index.js
@@ -1,9 +1,23 @@
 let username = undefined;
 
 let boller = false;
-const buttons = document.getElementById('btn')
+const button = document.getElementById('btn')
+const button1 = document.getElementById('btn1')
+// const buttons = document.querySelectorAll("#btn")
 
-buttons.addEventListener('click', e =>{
+button.addEventListener('click', e =>{
+    if (boller === false)
+{
+    username = prompt('Введите имя', username);
+    alert('Я это сделал, а ' + username +  ' тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”.')
+    boller = true;
+}
+else {
+    alert('Я это сделал, а ' + username +  ' тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”.')
+}
+})
+
+button1.addEventListener('click', e =>{
     if (boller === false)
 {
     username = prompt('Введите имя', username);
